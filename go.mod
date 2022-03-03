cdbb999e9479645c8908ab72ca90bbb7c01e2559
Mark-Anthony Agbanu <markludde@gmail.com>
2022-03-03 20:12:49 +0000

    init logger

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/marklude/logger
+
+go 1.17
---
+++ logger.go
@@ -0,0 +1,38 @@
+package logger
+
+import (
+	"fmt"
+	"time"
+)
+
+type logger struct {
+	env string
+}
+
+var dt time.Time
+
+func Init() {
+	dt = time.Now()
+}
+
+type Logger interface {
+	Info(args ...interface{})
+	Warn(args ...interface{})
+	Error(args ...interface{})
+}
+
+func (logger) Info(args ...interface{}) {
+	fmt.Printf("[INFO] %s %s", append([]interface{}{dt.Format("01-02-2006 15:04:05")}, args...)...)
+}
+
+func (logger) Warn(args ...interface{}) {
+	fmt.Printf("[WARN] %s %s", append([]interface{}{dt.Format("01-02-2006 15:04:05")}, args...)...)
+}
+
+func (logger) Error(args ...interface{}) {
+	fmt.Printf("[ERROR] %s %s", append([]interface{}{dt.Format("01-02-2006 15:04:05")}, args...)...)
+}
+
+func NewLogger(env string) Logger {
+	return &logger{env: env}
+}

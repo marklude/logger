c4503b39e8c7ba749080d03c4b11160eed9dff74
Mark-Anthony Agbanu <markludde@gmail.com>
2022-03-06 23:40:44 +0000

    refactor logger

--- logger.go
+++ logger.go
@@ -11,20 +11,20 @@ type Logger struct {
 
 var dt time.Time
 
-func Init() {
+func init() {
 	dt = time.Now()
 }
 
 func (Logger) Info(args ...interface{}) {
-	fmt.Printf("[INFO] %s %s", append([]interface{}{dt.Format("01-02-2006 15:04:05")}, args...)...)
+	fmt.Printf("[INFO] %s %s\n", append([]interface{}{dt.Format("02-01-2006 15:04:05")}, args...)...)
 }
 
 func (Logger) Warn(args ...interface{}) {
-	fmt.Printf("[WARN] %s %s", append([]interface{}{dt.Format("01-02-2006 15:04:05")}, args...)...)
+	fmt.Printf("[WARN] %s %s\n", append([]interface{}{dt.Format("02-01-2006 15:04:05")}, args...)...)
 }
 
 func (Logger) Error(args ...interface{}) {
-	fmt.Printf("[ERROR] %s %s", append([]interface{}{dt.Format("01-02-2006 15:04:05")}, args...)...)
+	fmt.Printf("[ERROR] %s %s\n", append([]interface{}{dt.Format("02-01-2006 15:04:05")}, args...)...)
 }
 
 func NewLogger(env string) *Logger {

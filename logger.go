b6a3fb888b55274252124afc720058a526bf98c8
Mark-Anthony Agbanu <markludde@gmail.com>
2022-03-07 00:23:08 +0000

    refactor code

--- logger.go
+++ logger.go
@@ -13,17 +13,17 @@ var dt time.Time
 
 func (Logger) Info(args ...interface{}) {
 	dt = time.Now()
-	fmt.Printf("[INFO] %s \n", append([]interface{}{dt.Format("02-01-2006 15:04:05")}, args...)...)
+	fmt.Printf("[INFO] %v \n", append([]interface{}{dt.Format("02-01-2006 15:04:05")}, args...)...)
 }
 
 func (Logger) Warn(args ...interface{}) {
 	dt = time.Now()
-	fmt.Printf("[WARN] %s \n", append([]interface{}{dt.Format("02-01-2006 15:04:05")}, args...)...)
+	fmt.Printf("[WARN] %v \n", append([]interface{}{dt.Format("02-01-2006 15:04:05")}, args...)...)
 }
 
 func (Logger) Error(args ...interface{}) {
 	dt = time.Now()
-	fmt.Printf("[ERROR] %s \n", append([]interface{}{dt.Format("02-01-2006 15:04:05")}, args...)...)
+	fmt.Printf("[ERROR] %v \n", append([]interface{}{dt.Format("02-01-2006 15:04:05")}, args...)...)
 }
 
 func NewLogger(env string) *Logger {

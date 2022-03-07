1e43e1a3fe7e4cbf3ac62238113d42b7cc639765
Mark-Anthony Agbanu <markludde@gmail.com>
2022-03-07 00:43:51 +0000

    refactor code

--- logger.go
+++ logger.go
@@ -25,7 +25,7 @@ func (Logger) Warn(args ...interface{}) {
 
 func (Logger) Error(err error, args ...interface{}) {
 	dt = time.Now()
-	eris.Wrapf(err, "[ERROR] %v %v\n", append([]interface{}{dt.Format("02-01-2006 15:04:05")}, args...)...)
+	eris.Wrapf(err, "[ERROR] %v %v\n")
 }
 
 func NewLogger(env string) *Logger {
